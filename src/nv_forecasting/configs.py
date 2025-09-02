4d1cf38486d8f28d2e37a4558682fec41a120c29
Kacper Aleksander <kacper.aleksander.fin@gmail.com>
2025-09-02 11:59:13 +0200

    Streamline plotting and set up config file

---
+++ src/nv_forecasting/configs.py
@@ -0,0 +1,23 @@
+from sklearn.model_selection import TimeSeriesSplit
+
+DEFAULT_CONFIG = {
+    'target': 'y', # Target column name
+    'datetime_column_name': 'ds',
+    'columns_to_lag': ['y', 'count_of_orders'],
+    'columns_to_drop': ['y', 'count_of_orders'],
+
+    # Feature engineering
+    'lags': [182],
+
+    # Model training
+    'n_outer_splits': 4,
+    'n_inner_splits': 3,
+    'n_final_splits': 5,
+    'test_size': 182,
+    'scoring': 'neg_mean_squared_error'
+}
+
+# Data splits
+DEFAULT_CONFIG['outer_cv'] = TimeSeriesSplit(n_splits=DEFAULT_CONFIG['n_outer_splits'], test_size=DEFAULT_CONFIG['test_size'])
+DEFAULT_CONFIG['inner_cv'] = TimeSeriesSplit(n_splits=DEFAULT_CONFIG['n_inner_splits'], test_size=DEFAULT_CONFIG['test_size'])
+DEFAULT_CONFIG['final_cv'] = TimeSeriesSplit(n_splits=DEFAULT_CONFIG['n_final_splits'], test_size=DEFAULT_CONFIG['test_size'])
--- src/nv_forecasting/plots.py
+++ src/nv_forecasting/plots.py
@@ -1,29 +1,55 @@
 import pandas as pd
 import matplotlib
 import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+from sklearn.model_selection import TimeSeriesSplit
 
 color_palette = sns.color_palette()
 
-def plot_train_val(ax, target: str, train: pd.DataFrame, val: pd.DataFrame, **kwargs) -> None:
+def plot_train_val(ax: Axes, target: str, train: pd.DataFrame, val: pd.DataFrame, **kwargs) -> None:
     train[target].plot(ax=ax, color=color_palette[0], label='Train data', **kwargs)
     val[target].plot(ax=ax, color=color_palette[1], label='Validation data', **kwargs)
 
     _format_axis(ax)
 
-def plot_train_val_test(ax, target: str, train: pd.DataFrame, val: pd.DataFrame, test: pd.DataFrame, **kwargs) -> None:
-    test[target].plot(ax=ax, color=color_palette[4], label='Test data', **kwargs)
+def plot_train_val_test(ax: Axes, target: str, train: pd.DataFrame, val: pd.DataFrame, test: pd.DataFrame, **kwargs) -> None:
     plot_train_val(ax, target, train, val, **kwargs)
+    test[target].plot(ax=ax, color=color_palette[4], label='Test data', **kwargs)
 
     _format_axis(ax)
 
-def plot_prediction(ax, target: str, y_train: pd.DataFrame, y_test: pd.DataFrame, y_pred: pd.DataFrame, **kwargs) -> None:
+def plot_data_split(target: str, cv: TimeSeriesSplit, df: pd.DataFrame, figsize: tuple[int, int] = (20, 3), **kwargs) -> None:
+    n_splits = cv.get_n_splits()
+    
+    fig, ax = plt.subplots(n_splits, 1, figsize=(figsize[0], figsize[1]*n_splits), sharex=True)
+    
+    for fold, (train_idx, val_idx) in enumerate(cv.split(df)):
+        train, val = df.iloc[train_idx], df.iloc[val_idx]
+
+        plot_train_val(ax[fold], target, train, val, linewidth=0.5)
+
+def plot_nested_data_split(target: str, outer_cv: TimeSeriesSplit, inner_cv: TimeSeriesSplit, df: pd.DataFrame, figsize: tuple[int, int] = (20, 3), **kwargs) -> None:
+    n_outer_splits = outer_cv.get_n_splits()
+    n_inner_splits = inner_cv.get_n_splits()
+
+    fig, ax = plt.subplots(n_outer_splits*n_inner_splits, 1, figsize=(figsize[0], figsize[1]*n_outer_splits*n_inner_splits), sharex=True)
+
+    for outer_fold, (train_and_val_idx, test_idx) in enumerate(outer_cv.split(df)):
+        train_and_val, test = df.iloc[train_and_val_idx], df.iloc[test_idx]
+        for inner_fold, (train_idx, val_idx) in enumerate(inner_cv.split(train_and_val)):
+            train, val = df.iloc[train_idx], df.iloc[val_idx]
+            ax_idx = outer_fold * n_inner_splits + inner_fold
+            plot_train_val_test(ax[ax_idx], target, train, val, test, **kwargs)
+
+def plot_prediction(ax: Axes, target: str, y_train: pd.DataFrame, y_test: pd.DataFrame, y_pred: pd.DataFrame, **kwargs) -> None:
     y_train[target].plot(ax=ax, label='Train data', color=color_palette[0], **kwargs)
     y_test[target].plot(ax=ax, label='Test data', color=color_palette[4], **kwargs)
     y_pred[target].plot(ax=ax, label='Prediction', color=color_palette[1], style='--', **kwargs)
 
     _format_axis(ax)
 
-def _format_axis(ax):
+def _format_axis(ax: Axes) -> None:
     ax.get_yaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
     ax.set_xlabel('Date')
     ax.set_ylabel('$', rotation=0, labelpad=16)
